bfc39cdc6c3e18b01981d68a156d12f26504ca73
Angel Emilio <emilio.neyvi@gmail.com>
2025-09-17 20:58:13 -0600

    Pipes

--- src/app/heroes/heroes-list/heroes-list.component.ts
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Heroes } from '../heroes';
 
 @Component({
   selector: 'app-heroes-list',
@@ -8,37 +9,43 @@ import { Component } from '@angular/core';
 })
 export class HeroesListComponent {
 
-  heroes:any[]=[
+  listFilter:string='';
+  muestraImg:boolean=true;
+  showImage(){
+    this.muestraImg=!this.muestraImg
+  }
+
+  heroes:Heroes[]=[ //Establecer el tipado en cada uno de los datos
     {
-      "imagen":"https://dragonball-api.com/characters/goku_normal.webp",
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
       nombre:"Goku",
       descripcion:"Kakaroto",
       race:"Saiyan",
       k1:90
     },
     {
-      "imagen":"https://dragonball-api.com/characters/vegeta_normal.webp",
+      imagen:"https://dragonball-api.com/characters/vegeta_normal.webp",
       nombre:"Vegeta",
       descripcion:"Principe de los saiyan",
       race:"Saiyan",
       k1:85000
     },
     {
-      "imagen":"https://dragonball-api.com/characters/picolo_normal.webp",
+      imagen:"https://dragonball-api.com/characters/picolo_normal.webp",
       nombre:"Piccolo",
       descripcion:"Z-fighter",
       race:"Nameikan",
       k1:5000
     },
     {
-      "imagen":"https://dragonball-api.com/characters/bulma.webp",
+      imagen:"https://dragonball-api.com/characters/bulma.webp",
       nombre:"Bulma",
       descripcion:"Esposa de Vegeta",
       race:"Humano",
       k1:0
     },
     {
-      "imagen":"https://dragonball-api.com/characters/Freezer.webp",
+      imagen:"https://dragonball-api.com/characters/Freezer.webp",
       nombre:"Freezer",
       descripcion:"Army of Frieza",
       race:"Frieza",
---
+++ src/app/heroes/heroes.ts
@@ -0,0 +1,7 @@
+export interface Heroes {
+  imagen:string,
+      nombre:string,
+      descripcion:string,
+      race:string,
+      k1:number
+}
